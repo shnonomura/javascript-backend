f5515426829cb3efcd4c2c157200684b503a508c
unknown <shnonomura@gmail.com>
2021-12-26 22:33:24 -0300

    comparacoes e ternario

---
+++ comparacoes.js
@@ -0,0 +1,17 @@
+// == (comparacao implicita)
+const numero = 5;
+const texto = "5";
+
+//console.log(numero == texto)
+//console.log(numero === texto)
+
+// typeof
+console.log(typeof numero)
+console.log(typeof texto)
+
+// ==  só compara valor
+// === compara o valor e o tipo do dado
+
+// conversao de forma  explicita
+Number()
+String()
